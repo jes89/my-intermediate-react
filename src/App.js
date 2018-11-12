a13917bce5f6a517ed4c89ba8977821f58078c1a
jes89 <hiter00@naver.com>
2018-11-12 15:05:30 +0900

    commit before eject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MyComponent from './MyComponent';
+import Button from './components/Button';
+import StyleButton from './components/styleButton';
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <MyComponent name="React" ref={(ref) => {this.MyComponent = ref}} ></MyComponent>
+        <Button>버튼</Button>
+        <StyleButton big={true}>버튼</StyleButton>
       </div>
     );
   }
